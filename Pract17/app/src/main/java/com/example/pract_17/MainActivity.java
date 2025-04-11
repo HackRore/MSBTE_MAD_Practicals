3fce02cf65183d48a89c09126fbe3f16d9093155
HackRore <hackrore@gmail.com>
2025-04-11 19:55:02 +0530

    Practical No. 17 added

---
+++ Pract17/app/src/main/java/com/example/pract_17/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.pract_17;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
